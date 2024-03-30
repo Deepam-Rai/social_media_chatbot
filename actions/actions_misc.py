6bcba6f338fb1b9b701d1cdbe2ba05681da0b4bf
deepam <deepam@chatowl.com>
2024-03-30 23:23:51 +0530

    Implemented custom connector for telegram that gives reasonable response for invalid api input.

--- actions/actions_misc.py
+++ actions/actions_misc.py
@@ -3,6 +3,10 @@ from actions.utils import *
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from actions.constants import *
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class ActionTellJoke(Action):
@@ -13,6 +17,24 @@ class ActionTellJoke(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         joke = get_joke()
-        dispatcher.utter_message(response="utter_tell_joke")
+        # dispatcher.utter_message(response="utter_tell_joke")
         dispatcher.utter_message(text=joke)
+        return []
+
+
+class ActionTest(Action):
+    """
+    For testing functionalities.
+    """
+
+    def name(self) -> Text:
+        return "action_test"
+
+    def run(
+        self,
+        dispatcher: "CollectingDispatcher",
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        logger.debug("Test action run.")
         return []
